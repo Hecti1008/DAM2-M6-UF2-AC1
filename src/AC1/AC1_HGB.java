714db73631d06dc2e9bd393027fd3891b9b56dfe
Hecti1008 <59198132+Hecti1008@users.noreply.github.com>
2020-11-29 22:48:42 +0100

    Conexio a mysql

--- src/AC1/AC1_HGB.java
+++ src/AC1/AC1_HGB.java
@@ -10,12 +10,11 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.*;
-import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.Statement;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -23,12 +22,27 @@ import com.mysql.jdbc.Statement;
  * @author Hector Garzon
  */
 public class AC1_HGB {
-
-    /**
-     * @param args the command line arguments
-     */
+    public static final String URL = "jbdc:mysql://localhost:3306/AC1_UF2_M6";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Connection connection = null;
+            connection = connection();
+    }
+    
+    public static Connection connection(){
+        Connection connection = null;
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            JOptionPane.showMessageDialog(null, "Conexio establerta");
+        } catch  (Exception e) {
+            System.out.println("e");
+        }
+        return connection;
     }
     
 }
